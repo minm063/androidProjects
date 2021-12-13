51fd9f834753ce50fcc4ec3f89dee2fdfb753d1e
minm063 <jangmin063@gmail.com>
2021-12-14 02:24:17 +0900

    #38 구글 로그인 연동

---
+++ GoogleLoginExample/app/src/main/java/com/example/googleloginexample/ResultActivity.java
@@ -0,0 +1,34 @@
+package com.example.googleloginexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private TextView tv_result;
+    private ImageView iv_profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        // MainActivity 로부터 닉네임, 프로필 사진 Url을 전달 받음
+        String nickName = intent.getStringExtra("nickName");
+        String photoUrl = intent.getStringExtra("photoUrl");
+
+        tv_result = findViewById(R.id.tv_result);
+        tv_result.setText(nickName);
+
+        iv_profile = findViewById(R.id.iv_profile);
+        // load photoUrl into iv_profile
+        Glide.with(this).load(photoUrl).into(iv_profile); // 이미지 로딩을 돕는 Glide
+    }
+}
